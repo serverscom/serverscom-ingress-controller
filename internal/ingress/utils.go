a06199ae3683fc448271ab6a9703eb0c609f99cd
Oleg Isakov <ion@servers.com>
2024-05-29 18:42:37 +0300

    handle default ingress class in flags

--- internal/ingress/utils.go
+++ internal/ingress/utils.go
@@ -3,15 +3,11 @@ package ingress
 import v1 "k8s.io/api/networking/v1"
 
 const (
-	IngressClassKey       = "kubernetes.io/ingress.class"
-	DefaultScIngressClass = "serverscom"
+	IngressClassKey = "kubernetes.io/ingress.class"
 )
 
-// IsScIngress checks if Ingress belongs to a specified class or DefaultScIngressClass if class empty
+// IsScIngress checks if Ingress belongs to a specified class
 func IsScIngress(i *v1.Ingress, class string) bool {
-	if class == "" {
-		class = DefaultScIngressClass
-	}
 	if i.Spec.IngressClassName != nil {
 		return *i.Spec.IngressClassName == class
 	}
